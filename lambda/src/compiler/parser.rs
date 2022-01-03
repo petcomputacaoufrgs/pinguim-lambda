1e5cb3b8b46d776e9c76c62ba50af29b8c7c5b6f
Nicolle Favero <nicollepf@gmail.com>
2022-01-03 11:29:19 -0300

    parsing

--- lambda/src/compiler/parser.rs
+++ lambda/src/compiler/parser.rs
@@ -2,22 +2,37 @@ pub mod ast;
 
 use crate::compiler::lexer::token::{Token, TokenType};
 use ast::{Value, Program};
+use super::error::{Diagnostics, Error};
 use indexmap::IndexMap;
 
+/// Cria uma estrutura Parser e parsa a lista de tokens para um programa
+/// 
+/// - `tokens`: vetor de tokens
+/// - `diagnostics`: vetor que armazena erros coletados durante a compilação 
 pub fn parse(
-    tokens: Vec<Token>
+    tokens: Vec<Token>,
+    diagnostics: &mut Diagnostics,
 ) -> Option<Value> {
     todo!()
 }
 
+#[derive(Debug)]
+/// Estrutura responsável por parar o parser em situações críticas
 struct Abort;
 
 struct Parser {
+    ///
+    /// - `tokens`: vetor de tokens a serem parsados
     tokens: Vec<Token>,
+    ///
+    /// - `curr_token`: índice do token que está sendo parsado
     curr_token: usize
 }
 
 impl Parser {
+    /// Cria uma nova estrutura de Parser
+    /// 
+    /// - `tokens`: vetor de tokens
     fn new(tokens: Vec<Token>) -> Self {
         Self {
             tokens,
@@ -25,21 +40,30 @@ impl Parser {
         }
     }
     
+    /// Pega o token o qual está sendo parsado no momento dado seu índice `curr_token`
     fn current(&self) -> Option<&Token> {
         self.tokens.get(self.curr_token)
     }
 
+    /// Pega o token o qual está sendo parsado no momento e garante que ele exista
+    /// 
+    /// - `diagnostics`: vetor que armazena erros coletados durante a compilação
     fn require_current(&self) -> Result<&Token, Abort> {
         match self.current() {
-            Some(&token) => Ok(&token),
+            Some(token) => Ok(token),
             None => Err(Abort)
         }
     }
 
+    /// Incrementa o índice para o próximo token
     fn  next(&mut self) {
         self.curr_token += 1;
     }
 
+    /// Confere se o próximo token é do tipo esperado, adicionando erro no diagnóstico quando não for
+    /// 
+    /// - `expected_type`: tipo de token que é esperado encontrar
+    /// - `diagnostics`: vetor que armazena erros coletados durante a compilação
     fn expect(&mut self, expected_type: TokenType) -> Result<(), Abort> {
         let token = self.require_current()?;
 
@@ -53,6 +77,10 @@ impl Parser {
         Ok(())
     }
 
+    /// Confere se o próximo token é do tipo esperado, retornando true se for, false se não for
+    /// 
+    /// - `expected_type`: tipo de token esperado
+    /// - `diagnostics`: vetor que armazena erros coletados durante a compilação
     fn check_expect(&mut self, expected_type: TokenType) -> Result<bool, Abort> {
         let token = self.require_current()?;
 
@@ -64,11 +92,14 @@ impl Parser {
         }
     }
 
+    /// Faz o parse do vetor de tokens em um programa
+    /// 
+    /// - `diagnostics`: vetor que armazena erros coletados durante a compilação
     fn parse_program(&mut self,) -> Result<Option<Program>, Abort> {
-        let mut program: ast::Value;
-        let mut let_option: IndexMap<String, Value>;
-        let mut let_declared = false;
-        let mut in_declared = false;
+        let mut program: ast::Value;                        // nodo inicial do programa
+        let mut let_option: IndexMap<String, Value>;        // mapeamento das definições, se existir bloco let
+        let mut let_declared = false;                       // controle da keyword let no código lambda
+        let mut in_declared = false;                        // controle da keyword in no código lambda
 
         while let Some(token) = self.current() {
             let token_span = token.span;
@@ -78,12 +109,14 @@ impl Parser {
                     if !let_declared {
                         let_option = self.parse_let()?;
                         let_declared = true;
+                    } else {
+                        // erro: keyword let já foi declarada
                     }
                 },
                 TokenType::Lambda => {
 
                 },
-                _ => //errouuu
+                _ => () //errouuu
             }
         }
 
@@ -91,8 +124,18 @@ impl Parser {
     }
 
     fn parse_let(&mut self) -> Result<IndexMap<String, Value>, Abort> {
+        self.next();
 
-        // Basicamente vai passar por cada expressão (separador de expressões: ponto e vírgula) e chamar outro método que parsa 1 expressão por vez
+        while let Some(token) = self.current() {
+            if token.token_type == TokenType::In {
+                // error: EmptyLetBlock
+                todo!()
+            } else {
+                self.parse_expr();
+            }
+        }
+
+        self.next();
         todo!()
     }
 
@@ -106,23 +149,25 @@ impl Parser {
         if token.token_type == TokenType::Identifier {
             let expr_name = token.content.clone();
 
-            self.next();
             Ok(Some(expr_name))
+
         } else {
             let expected_types = vec![TokenType::Identifier];
-            // diagnostics.raise(Error::new(
-            //      UnexpectedToken { expected_types },
-            //      token.span,
-            // ))
+            //error: UnexpectedToken
             todo!()
         }
     }
 
     fn parse_func_expr(&mut self) -> Result<Value, Abort> {
         let mut expr_value: Value;
+        
+        self.next();
 
         self.expect(TokenType::Equal);
+        self.next();
 
-        todo!()
+        if Some(token) = self.current() {
+            expr_value = Value::
+        }
     }
 }
